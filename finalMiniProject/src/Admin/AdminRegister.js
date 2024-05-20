5c2a171927eeaf69468647e8ddc918e772280b8d
SagarTeli1999 <wreckerteli1999@gmail.com>
2024-05-20 11:11:00 +0530

    first commit

---
+++ finalMiniProject/src/Admin/AdminRegister.js
@@ -0,0 +1,153 @@
+import React, { useState } from 'react'
+// import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+export default function AdminRegister() {
+    const [user, setUser] = useState({
+        Name:"",
+        email:"",
+        password:""
+      });
+  
+      const { Name, email, password } = user;
+    
+      const onInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+      };
+    
+      const onSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post("http://localhost:8080/user", user);
+        console.log(user);
+        console.log("success");
+      };
+  
+    return (
+      <div>
+      {/* Section: Design Block */}
+      <section className="text-center">
+        {/* Background image */}
+        <div
+          className="p-5 bg-image"
+          style={{
+            backgroundImage:
+              'url("https://mdbootstrap.com/img/new/textures/full/171.jpg")',
+            height: "300px",
+          }}
+        />
+        {/* Background image */}
+        <div
+          className="card mx-4 mx-md-5 shadow-5-strong"
+          style={{
+            marginTop: "-100px",
+            background: "hsla(0, 0%, 100%, 0.8)",
+            backdropFilter: "blur(30px)",
+          }}
+        >
+          <div className="card-body py-5 px-md-5">
+            <div className="row d-flex justify-content-center">
+              <div className="col-lg-8">
+                <h2 className="fw-bold mb-5">Admin Registration</h2>
+                <form onSubmit={(e) => onSubmit(e)}>
+                  {/* 2 column grid layout with text inputs for the first and last names */}
+                  
+                  <div className="form-outline mb-4">
+                    <input
+                       type="text"
+                       id="form3Example1"
+                       className="form-control"
+                       name="firstName"
+                       value={Name} 
+                       onChange={(e) => onInputChange(e)}
+                    />
+                    <label className="form-label" htmlFor="form3Example3">
+                      Name
+                    </label>
+                  </div>
+                   
+                   
+                  <div className="form-outline mb-4">
+                    <input
+                      type="email"
+                      id="form3Example3"
+                      className="form-control"
+                      name="email"
+                      value={email} 
+                      onChange={(e) => onInputChange(e)}
+                    />
+                    <label className="form-label" htmlFor="form3Example3">
+                      Email address
+                    </label>
+                  </div>
+  
+                  
+  
+                 
+  
+                  
+  
+                  {/* Password input */}
+                  <div className="form-outline mb-4">
+                    <input
+                      type="password"
+                      id="form3Example4"
+                      className="form-control"
+                      name="password"
+                      value={password} 
+                      onChange={(e) => onInputChange(e)}
+                    />
+                    <label className="form-label" htmlFor="form3Example4">
+                      Password
+                    </label>
+                  </div>
+  
+                  {/* Submit button */}
+                  <a href='google.com'>
+                  <button
+                    type="submit"
+                    className="btn btn-primary btn-block mb-4"
+                    
+                  >
+                    Sign up
+                  </button>
+                  </a>
+                  
+                  {/* Register buttons */}
+                  <div className="text-center">
+                 
+                    <button
+                      type="button"
+                      className="btn btn-link btn-floating mx-1"
+                    >
+                      <i className="fab fa-facebook-f" />
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-link btn-floating mx-1"
+                    >
+                      <i className="fab fa-google" />
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-link btn-floating mx-1"
+                    >
+                      <i className="fab fa-twitter" />
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-link btn-floating mx-1"
+                    >
+                      <i className="fab fa-github" />
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Section: Design Block */}
+    </div>
+    )
+}
+
